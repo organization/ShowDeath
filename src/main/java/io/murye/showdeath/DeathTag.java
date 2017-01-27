26b913717824079d9718b023a66eb7ecdbbe3a2e
MuRye <dreamaker7770@gmail.com>
2017-01-27 16:21:44 +0900

    Debug

--- src/main/java/io/murye/showdeath/DeathTag.java
+++ src/main/java/io/murye/showdeath/DeathTag.java
@@ -5,29 +5,34 @@ import cn.nukkit.Server;
 import cn.nukkit.scheduler.AsyncTask;
 import milk.tagmanager.text.Tag;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by murye on 1/27/17.
  */
-public class DeathTag extends Tag{
+public class DeathTag {
+    public static Set<DeathTag> tags = new HashSet<>();
     protected Long since = new Long(0);
     protected Player player;
+    protected Tag tag;
 
     public DeathTag(Player player) {
-        super(player.getPosition(), "0분 전에 " + player.getName() + "이(가) 사망", new Long(60 * 10));
+        this.tag = new Tag(player.getPosition(), "0분 전에 " + player.getName() + "이(가) 사망", new Long(60 * 10));
         this.player = player;
+        DeathTag.tags.add(this);
         Server.getInstance().getScheduler().scheduleRepeatingTask(new AsyncTask() {
             @Override
             public void onRun() {
-                if(Server.getInstance().getOnlinePlayers().isEmpty()) {
-                    return;
-                }
-
-                for(Tag tag : DeathTag.list.values()) tag.onUpdate();
+                for(DeathTag tag : DeathTag.tags) tag.onUpdate();
             }
         }, 20 * 60, true);
     }
 
     public void onUpdate() {
-        this.setText(++this.since + "분 전에 " + this.player.getName() + "이(가) 사망");
+        this.tag.setText(++this.since + "분 전에 " + this.player.getName() + "이(가) 사망");
+        if(this.since >= 30)
+            DeathTag.tags.remove(this.tag);
     }
 }
